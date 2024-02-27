29d06281247eb4b76500ba6037a3858b1ddfba5f
Antoine <antoine.reib3l@gmail.com>
2024-02-27 13:28:57 +0100

    typo

--- bejeweled/components/ProgressBar.js
+++ bejeweled/components/ProgressBar.js
@@ -1,9 +1,9 @@
 import { View, StyleSheet, Text, Button, Alert } from 'react-native'
 import { useState, useEffect, useContext } from 'react';
-import {PointsContext} from '../context/PointsContext';
+import { PointsContext } from '../context/PointsContext';
 
 function ProgressBar() {
-    const {points, setPoints} = useContext(PointsContext)
+    const { points, setPoints } = useContext(PointsContext)
     const [score, setScore] = useState(0)
     const [progressScore, setProgressScore] = useState(50)
     const [maxScore, setMaxScore] = useState(100)
@@ -12,25 +12,22 @@ function ProgressBar() {
     useEffect(() => {
         let timer = setInterval(() => {
             setProgressScore(progressScore => {
-                if (progressScore - 3*level <= 0) {
+                if (progressScore - 3 * level <= 0) {
                     clearInterval(timer)
                     Alert.alert('Game Over', 'Vous avez perdu')
                     return 0
                 }
-                return progressScore - 3*level})
-            console.log('interval')
+                return progressScore - 3 * level
+            })
         }, 1000)
         return () => {
             clearInterval(timer)
-            console.log('cleanup interval')
         }
     }, [level])
 
     useEffect(() => {
         setProgressScore(progressScore => progressScore + points * level)
         setScore(score + points * level)
-
-        console.log('render')
 
         if (progressScore >= maxScore) {
             setMaxScore(maxScore * 2)
